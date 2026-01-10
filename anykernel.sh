5370ae2373c67ccf4da93fbe72a7291c7e5350db
Vhmit <viktor.9630@protonmail.com>
2026-01-10 12:12:21 -0300

    ak3: Adapt script motolegacy for jeter
    Signed-off-by: Vhmit <viktor.9630@protonmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -12,15 +12,15 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=deen
-device.name2=deen_sprout
-supported.versions=
+device.name1=jeter
+device.name2=aljeter
+supported.versions=10.0
 '; } # end properties
 
 # shell variables
-block=/dev/block/by-name/boot;
-is_slot_device=1;
-ramdisk_compression=lzma;
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
